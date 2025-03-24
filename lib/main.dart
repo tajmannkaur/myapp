f55aa58925d90ae191abd4e69ef4a71273ea4a68
tajmannkaur <tajmannkamboj08@gmail.com>
2025-03-24 21:14:49 +0000

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,15 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MainApp());
 }
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage());
+    return MaterialApp(home: HomePage());
   }
 }
 
@@ -25,10 +23,26 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(backgroundColor: Colors.blue),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [FlutterLogo(size: 50)],
+        ),
+      ),
       drawer: Drawer(),
-      body: Container(color: Colors.white),
+      body: Container(
+        width: 255,
+        height: 300,
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
+        ),
+      ),
       bottomNavigationBar: BottomAppBar(),
     );
   }
